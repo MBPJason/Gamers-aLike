4b07c760673dcc12bd8efe7c803394eb02662e11
Jason Ozulumba <cjozulumba@gmail.com>
2020-11-24 00:31:48 -0500

    Built out starter boilerplate for 'Gamers-aLike' MERN app

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my-mern", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -25,6 +27,10 @@ connection.on("connected", () => {
 
 connection.on("error", (err) => {
   console.log("Mongoose connection error: ", err);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.get("/api/config", (req, res) => {
