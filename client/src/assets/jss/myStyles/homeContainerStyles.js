d9adf356ce5100fba990a897fbe95d71e733c2a6
Jason Ozulumba <cjozulumba@gmail.com>
2021-05-14 06:18:14 -0400

    Still working on cards to display properly

--- client/src/assets/jss/myStyles/homeContainerStyles.js
+++ client/src/assets/jss/myStyles/homeContainerStyles.js
@@ -4,6 +4,22 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
   },
+  cardSize: {
+    maxHeight: 400,
+    maxWidth: 304,
+    margin: theme.spacing(3, 2),
+    borderRadius: 0,
+    position: 'relative',
+  },
+  font: {
+    position: "absolute",
+    top: "20%",
+    width: "100%",
+    textAlign: "center",
+    color: "white",
+    backgroundColor: "none",
+    
+  }
 }));
 
 export default useStyles;
